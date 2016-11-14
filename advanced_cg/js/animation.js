91e20a776da14113ead55e51ef3b8f1f79cc8fd9
Hammad Haleem <hammadhaleem@gmail.com>
2016-11-14 12:14:13 +0800

    memory issues

--- advanced_cg/js/animation.js
+++ advanced_cg/js/animation.js
@@ -80,7 +80,7 @@ function render() {
     if (music_is_active ) {
     //     var nBarHalf = Math.ceil(nBar / 2)
         histo = sound.makeHistogram(30);
-        amplitude = 0 ;// sound.amplitude();
+        amplitude =d.amplitude();
         console.log([render_counter, amplitude , histo , Math.sin(time / 2000)]);
 
 
